4dcf78d8e8e58797e143142bcb9218a037fbc50a
David <davidarutyunov77@gmail.com>
2024-09-21 22:29:49 +0200

    add expirience section with tabs

---
+++ app/js/main.js
@@ -0,0 +1,60 @@
+(function (){
+  const headerBurgerOpen = document.querySelector('.header__burger');
+  const burgerNavigation = document.querySelector('.header__nav');
+  const headerBurgerClose = document.querySelector('.header__nav-close');
+
+  headerBurgerOpen.addEventListener('click', event => (
+    burgerNavigation.classList.add('active')
+  ))
+
+  headerBurgerClose.addEventListener('click', event => (
+    burgerNavigation.classList.remove('active')
+  ))
+})();
+
+(function () {
+  const tabItems = document.querySelectorAll('.expirience__tab-link');
+  const tabContent = document.querySelectorAll('.expirience__content-item');
+
+  tabItems.forEach((tab, index) => {
+    tab.addEventListener('click', (event) => {
+      event.preventDefault();
+
+      tabContent.forEach(content => {
+        content.classList.remove('active');
+      });
+      tabItems.forEach(tab => {
+        tab.classList.remove('active');
+      });
+
+      tabContent[index].classList.add('active');
+      tabItems[index].classList.add('active');
+    })
+  })
+})();
+
+
+// (function () {
+//   const tabItems = document.querySelectorAll('.expirience__tab-link');
+//   const tabContent = document.querySelectorAll('.expirience__content-item');
+
+//   tabItems.forEach((tab, index) => {
+//     tab.addEventListener('click', (event) => {
+//       event.preventDefault();
+
+//       // Удаляем класс 'active' у всех элементов контента
+//       tabContent.forEach(content => {
+//         content.classList.remove('active');
+//       });
+
+//       // Удаляем класс 'active' у всех вкладок
+//       tabItems.forEach(tab => {
+//         tab.classList.remove('active'); // Здесь ошибка была
+//       });
+
+//       // Добавляем класс 'active' выбранной вкладке и соответствующему контенту
+//       tabContent[index].classList.add('active');
+//       tabItems[index].classList.add('active');
+//     });
+//   });
+// })();
